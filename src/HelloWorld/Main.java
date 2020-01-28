f79855963afc8465fb2f48e57856fb5df0f11968
olaalnarkailchi <meine.apps92@gmail.com>
2020-01-28 09:19:00 +0100

    ola test

--- src/HelloWorld/Main.java
+++ src/HelloWorld/Main.java
@@ -22,6 +22,7 @@ public class Main {
         System.out.println("Hello World 13");
         System.out.println("Hello World 14");
         System.out.println("Hi 15");
+        System.out.println("neu test");
 
     }
 }
